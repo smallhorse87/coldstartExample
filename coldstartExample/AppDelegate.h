cf74d8e506663d835c258f4e68a128e4eb11609f
chenxiaosong <smallhorse87@163.com>
2019-01-14 15:04:09 +0800

    Initial Commit

---
+++ coldstartExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coldstartExample
+//
+//  Created by chenxiaosong on 2019/1/14.
+//  Copyright © 2019年 chenxiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
